5285659af265e8e507733f51f613e4cbdf5fe3d0
abdullahalnoor <aalnoor74@gmail.com>
2018-08-15 23:40:07 +0600

    google login

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Socialite;
+use Auth;
+use App\User;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+      /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+       $user =  Socialite::driver('google')->stateless()->user();
+
+        return $user->email;
+    }
+
+
+}
---
+++ config/services.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => env('SES_REGION', 'us-east-1'),
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+     'google' => [
+        'client_id' => "117860130247-bg2vpt19ll1mjkrfcc69ic9o2p6cmdt7.apps.googleusercontent.com",      
+        'client_secret' => "PYVkqr9_9GRYbHi81ByB0jsu", 
+        'redirect' => "http://socialite-laravel.com/login/google/callback", 
+    ],
+
+    // 'facebook' => [
+    //     'client_id' => env('FACEBOOK_CLIENT_ID'),      
+    //     'client_secret' => env('FACEBOOK_CLIENT_SECRET'), 
+    //     'redirect' => env('FACEBOOK_URL'), 
+    // ],
+//     FACEBOOK_CLIENT_ID="411198016074872"
+// FACEBOOK_CLIENT_SECRET="8dc3b0d1510b3b59f4e8830bca0a1e26"
+// FACEBOOK_URL="http://socialite-laravel.com/home"
+
+];
